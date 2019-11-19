b235f073cc9a2e768d3480c0cfa9e843dbaf7c84
hainguyen <hai@iwa.fi>
2019-11-20 00:44:55 +0700

    authorize current user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,8 @@ class ApplicationController < ActionController::Base
   def current_user
     @current_user ||= User.find_by(id: session[:user_id])
   end
+
+  def authorize
+    redirect_to '/' unless current_user
+  end
 end
--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,4 +1,6 @@
 class VideosController < ApplicationController
+  before_action :authorize
+
   def index
   end
 
