ce511e0de513f65cb3b43deacd4cad25b4c9a815
hainguyen <hai@iwa.fi>
2019-11-19 12:05:08 +0700

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   resources :users, only: [:new, :create]
-  resources :sessions, only: [:new, :create] do
-  end
+  resources :sessions, only: [:new, :create]
 
   root 'sessions#visitor'
+
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
 end
