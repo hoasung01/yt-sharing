f16d57cfa9f47c985e1ddc7d4b5dc0c62a6a120b
hainguyen <hai@iwa.fi>
2019-11-20 00:03:50 +0700

    polish sharing movie page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   resources :users, only: [:new, :create]
   resources :sessions, only: [:new, :create]
-  resources :videos, only: :index
+  resources :videos, only: [:index, :create]
+
   root 'sessions#visitor'
   post :login, to: 'sessions#create'
   delete :logout, to: 'sessions#destroy'
