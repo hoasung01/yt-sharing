20b3494d752222023a184e17419bb130ee0fe7af
hainguyen <hai@iwa.fi>
2019-11-19 12:12:15 +0700

    add videos controllers and index action

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :users, only: [:new, :create]
   resources :sessions, only: [:new, :create]
+  resources :videos, only: :index
 
   root 'sessions#visitor'
 
