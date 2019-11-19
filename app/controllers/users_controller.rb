1e948fedb1c71e759665f668744cde56bca0b865
hainguyen <hai@iwa.fi>
2019-11-19 12:05:25 +0700

    set session after user created

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,8 +4,10 @@ class UsersController < ApplicationController
   end
 
   def create
-   @user = User.new(user_params)
+    @user = User.new(user_params)
+
     if @user.save
+      session[:user_id] = @user.id
       redirect_to root_path, notice: t(:user_created)
     else
       render action: 'new'
