346217cfaae0759d7fb7d1abcb200b1812bd05e3
hainguyen <hai@iwa.fi>
2019-11-19 16:13:48 +0700

    update navbar html

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,6 @@ Rails.application.routes.draw do
   resources :users, only: [:new, :create]
   resources :sessions, only: [:new, :create]
   resources :videos, only: :index
-
   root 'sessions#visitor'
-
-  get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
 end
