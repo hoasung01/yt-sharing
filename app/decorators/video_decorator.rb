2cc98b2755d419a0c81a9ec886cdd347b724d0dc
hainguyen <hai@iwa.fi>
2019-11-20 18:39:19 +0700

    show youtube video

--- app/decorators/video_decorator.rb
+++ app/decorators/video_decorator.rb
@@ -1,10 +1,17 @@
 class VideoDecorator < SimpleDelegator
+  DEFAULT_ID = 'cuaKvwVmPaA'.freeze
+
   def model
     __getobj__
   end
 
   def shared_by
     user.email
+  end
+
+  def youtube_id
+    match_regex = model.link.match(/\?v=([\D\d]*)/)
+    match_regex ? match_regex[1]: DEFAULT_ID
   end
 
   private
