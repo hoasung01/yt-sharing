861985e760c97984eb60e85f87342ccc70a98d85
hainguyen <hai@iwa.fi>
2019-11-21 21:46:49 +0700

    update root routes and remove visitor page

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :sessions, only: [:new, :create]
   resources :videos, only: [:index, :create]
 
-  root 'sessions#visitor'
+  root 'videos#index'
   post :login, to: 'sessions#create'
   delete :logout, to: 'sessions#destroy'
   get :share, to: 'videos#new'
