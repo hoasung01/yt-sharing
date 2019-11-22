3ca9b3207990a4dd36f8533e18ecb2f58e595d75
hainguyen <hai@iwa.fi>
2019-11-22 15:24:39 +0700

    add test to validate presence of link

--- test/models/video_test.rb
+++ test/models/video_test.rb
@@ -4,4 +4,8 @@ class VideoTest < ActiveSupport::TestCase
   context 'associations' do
     should belong_to(:user)
   end
+
+  context 'validations' do
+    should validate_presence_of(:link)
+  end
 end
