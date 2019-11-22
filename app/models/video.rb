02a91f890d1031da0039e519c2dd6a9e307be409
hainguyen <hai@iwa.fi>
2019-11-22 23:37:31 +0700

    revise link regex

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,6 +1,6 @@
 class Video < ApplicationRecord
-  VALID_LINK_REGEX = /\A^(http(s)?:\/\/)?((w){3}.)?youtu(be|.be)?(\.com)?\/.+\Z/i
+  LINK_REGEX = /\A^(http(s)?:\/\/)?((w){3}.)?youtu(be|.be)?(\.com)?\/.+\Z/i
   paginates_per 5
   belongs_to :user
-  validates :link, presence: true, format: { with: VALID_LINK_REGEX }
+  validates :link, presence: true, format: { with: LINK_REGEX }
 end
