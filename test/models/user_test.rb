615b5681d0ba2295c546b1671ac7ede57d650e45
hainguyen <hai@iwa.fi>
2019-11-23 00:38:57 +0700

    update unit test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -8,5 +8,6 @@ class UserTest < ActiveSupport::TestCase
   context 'validations' do
     should validate_presence_of(:email)
     should validate_uniqueness_of(:email)
+    should allow_value('test@example.com').for(:email)
   end
 end
--- test/models/video_test.rb
+++ test/models/video_test.rb
@@ -8,5 +8,6 @@ class VideoTest < ActiveSupport::TestCase
   context 'validations' do
     should validate_presence_of(:link)
     should validate_uniqueness_of(:link).scoped_to(:user_id)
+    should allow_value('https://www.youtube.com/watch?v=U7-656WLjk8').for(:link)
   end
 end
