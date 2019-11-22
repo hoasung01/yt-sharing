8c4d9f167f78b756d9edb1493cf0867f999caa2b
hainguyen <hai@iwa.fi>
2019-11-22 16:37:41 +0700

    add validate format of link and add screenshot images

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,5 +1,6 @@
 class Video < ApplicationRecord
   paginates_per 5
   belongs_to :user
-  validates :link, presence: true
+  validates_presence_of :link
+  validates_format_of :link, with: /\A^(http(s)?:\/\/)?((w){3}.)?youtu(be|.be)?(\.com)?\/.+\Z/i
 end
